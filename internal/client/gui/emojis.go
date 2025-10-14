7028142f5007c5f0142439bc47103905230724b8
n9hquan <quan.nguyen.220066@student.fulbright.edu.vn>
2025-10-14 11:15:29 +0700

    feat: add new emojis and emoji tabs

--- internal/client/gui/emojis.go
+++ internal/client/gui/emojis.go
@@ -1,20 +1,36 @@
 package gui
 
-import (
-	"strings"
-)
-
-var emojiMap = map[string]string{
-	":)":     "😊",
-	"<3":     "❤️",
-	":like:": "👍",
-	"XD":     "😂",
-	":(":     "😢",
-}
-
-func ConvertEmojis(text string) string {
-	for code, emoji := range emojiMap {
-		text = strings.ReplaceAll(text, code, emoji)
-	}
-	return text
+var emojiMap = map[string]map[string]string{
+	"😊": {
+		":)": "😊", ":(": "😢", "XD": "😂", ":D": "😄", "<3": "❤️", "</3": "💔",
+		":P": "😛", ";)": "😉", ":o": "😲", ":/": "😕", ":|": "😐", ":angry:": "😠", 
+		":cry:": "😭", ":sick:": "🤢", ":sleepy:": "😴", ":cool:": "😎", ":confused:": "😕",
+	},
+	"👍": {
+		":like:": "👍", ":dislike:": "👎", ":clap:": "👏", ":pray:": "🙏",
+		":ok:": "👌", ":fist:": "✊", ":wave:": "👋", ":muscle:": "💪",
+		":point_up:": "☝️", ":point_down:": "👇", ":point_left:": "👈", ":point_right:": "👉",
+	},
+	"🐱": {
+		":dog:": "🐶", ":cat:": "🐱", ":fox:": "🦊", ":lion:": "🦁", ":tiger:": "🐯",
+		":bear:": "🐻", ":panda:": "🐼", ":unicorn:": "🦄", ":rabbit:": "🐰",
+		":frog:": "🐸", ":monkey:": "🐵", ":elephant:": "🐘", ":koala:": "🐨",
+		":penguin:": "🐧", ":whale:": "🐳", ":dolphin:": "🐬", ":octopus:": "🐙",
+		":fish:": "🐟", ":butterfly:": "🦋", ":snail:": "🐌", ":bee:": "🐝", ":ant:": "🐜",
+	},
+	"🍕": {
+		":apple:": "🍎", ":banana:": "🍌", ":cherry:": "🍒", ":coffee:": "☕",
+		":pizza:": "🍕", ":burger:": "🍔", ":fries:": "🍟", ":cake:": "🍰",
+		":icecream:": "🍨", ":donut:": "🍩", ":cookie:": "🍪", ":beer:": "🍺", ":wine:": "🍷",
+	},
+	"☀️": {
+		":sun:": "☀️", ":moon:": "🌙", ":star:": "⭐", ":fire:": "🔥",
+		":rainbow:": "🌈", ":gift:": "🎁", ":music:": "🎵", ":game:": "🎮",
+	},
+	"🏀": {
+		":soccer:": "⚽", ":basketball:": "🏀", ":football:": "🏈", ":tennis:": "🎾",
+		":baseball:": "⚾", ":golf:": "⛳", ":volleyball:": "🏐", ":pingpong:": "🏓",
+		":hockey:": "🏒", ":ski:": "🎿", ":swim:": "🏊", ":bike:": "🚴", ":weight:": "🏋️",
+		":martial_arts:": "🥋", ":fencing:": "🤺", ":bowling:": "🎳", ":cricket:": "🏏",
+	},
 }
